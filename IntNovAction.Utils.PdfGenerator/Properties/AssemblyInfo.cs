5f7c08038c674e2d0812b66a64a6172b5843df03
eblanco <eblanco@intnovaction.com>
2017-04-01 16:46:11 +0200

    Cambios para nuget

--- IntNovAction.Utils.PdfGenerator/Properties/AssemblyInfo.cs
+++ IntNovAction.Utils.PdfGenerator/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IntNovAction.Utils.PdfGenerator")]
-[assembly: AssemblyDescription("Excel exporter for lists")]
+[assembly: AssemblyDescription("Genera pdfs a partir de vistas MVC")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IntNovAction")]
 [assembly: AssemblyProduct("IntNovAction.Utils.PdfGenerator")]
