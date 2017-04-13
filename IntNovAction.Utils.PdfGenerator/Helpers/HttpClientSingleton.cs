464c5edc94c0ad7101239cec87ee7786859409a1
eblanco <eblanco@intnovaction.com>
2017-04-13 23:52:23 +0200

    Mejoras para que funcionen las imagenes con autenticacion con forms...

---
+++ IntNovAction.Utils.PdfGenerator/Helpers/HttpClientSingleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntNovAction.Utils.PdfGenerator.Helpers
+{
+    /// <summary>
+    /// Ingleton para el HttpClient.
+    /// Fusilado de @luisruizpavon : https://github.com/lurumad/aspnetcore-health/blob/master/src/AspNetCore.Health/Internal/HttpClientSingleton.cs
+    /// </summary>
+    public class HttpClientSingleton : HttpClient
+    {
+        private HttpClientSingleton(HttpClientHandler handler) : base(handler)
+        {
+
+        }
+
+        private static readonly Lazy<HttpClientSingleton> Lazy =
+        new Lazy<HttpClientSingleton>(() =>
+        {
+            var handler = new HttpClientHandler { UseCookies = false };
+            return new HttpClientSingleton(handler);
+        });
+
+        public static HttpClientSingleton Instance => Lazy.Value;
+
+        private HttpClientSingleton()
+        {
+            DefaultRequestHeaders.Add("cache-control", "no-cache");
+        }
+    }
+}
--- IntNovAction.Utils.PdfGenerator/Providers/DownloadImageProvider.cs
+++ IntNovAction.Utils.PdfGenerator/Providers/DownloadImageProvider.cs
@@ -1,18 +1,33 @@
-﻿using iTextSharp.tool.xml.pipeline.html;
+﻿using IntNovAction.Utils.PdfGenerator.Helpers;
+using iTextSharp.text;
+using iTextSharp.tool.xml.pipeline.html;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using iTextSharp.text;
-using System.Web.Hosting;
+using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Hosting;
 
 namespace IntNovAction.Utils.PdfGenerator.Providers
 {
     public class DownloadImageProvider : AbstractImageProvider
     {
+        private HttpClient httpClient;
+
+        public DownloadImageProvider()
+        {
+            httpClient = HttpClientSingleton.Instance;
+        }
+
+        public DownloadImageProvider(HttpClient customConfiguredClient)
+        {
+            httpClient = customConfiguredClient;
+        }
+
         public override string GetImageRootPath()
         {
             return null;
@@ -21,8 +36,9 @@ namespace IntNovAction.Utils.PdfGenerator.Providers
         public override Image Retrieve(string src)
         {
             Image image = null;
+            Uri uri;
 
-            if (Uri.TryCreate(src, UriKind.RelativeOrAbsolute, out Uri uri))
+            if (Uri.TryCreate(src, UriKind.RelativeOrAbsolute, out uri))
             {
                 // intentar obtener la imagen de forma local
                 if (!uri.IsAbsoluteUri || uri.LocalPath.Equals(src, StringComparison.InvariantCultureIgnoreCase))
@@ -39,16 +55,52 @@ namespace IntNovAction.Utils.PdfGenerator.Providers
                     }
                     catch (Exception ex)
                     {
-                        return null;
+                        Trace.TraceError(ex.ToString());
                     }
                 }
-
+            }
+            else
+            {
+                Trace.TraceError("Image uri error: {0}", src);
             }
 
             return image;
-
         }
 
+        private static void AddCookiesToMessage(string src, HttpRequestMessage message)
+        {
+            var uri = new Uri(src);
+            if (HttpContext.Current.Request.Url.Authority.Equals(uri.Authority, StringComparison.InvariantCultureIgnoreCase))
+            {
+                var cookies = new List<string>();
+
+                var sessionCookieName = "ASP.NET_SessionId";
+                SessionStateSection sessionStateSection = (SessionStateSection)ConfigurationManager.GetSection("system.web/sessionState");
+                if (sessionStateSection != null)
+                {
+                    sessionCookieName = sessionStateSection.CookieName;
+                }
+
+
+                foreach (var cookie in HttpContext.Current.Request.Cookies.AllKeys)
+                {
+                    if (cookie != sessionCookieName)
+                    {
+                        cookies.Add(string.Format("{0}={1}", cookie, HttpContext.Current.Request.Cookies[cookie].Value));
+                    }
+                }
+
+                message.Headers.Add("Cookie", string.Join("; ", cookies));
+            }
+        }
+
+
+
+        /// <summary>
+        /// Obtiene una imagen de un path local
+        /// </summary>
+        /// <param name="src"></param>
+        /// <returns></returns>
         private Image RetrieveLocalImage(string src)
         {
             var localPath = HostingEnvironment.MapPath(src);
@@ -56,22 +108,45 @@ namespace IntNovAction.Utils.PdfGenerator.Providers
             {
                 return Image.GetInstance(localPath);
             }
+            Trace.TraceError("Local file {0} not found", src);
             return null;
         }
 
+        /// <summary>
+        /// Obtiene una imagen de un path remoto
+        /// </summary>
+        /// <param name="src"></param>
+        /// <returns></returns>
         private Image RetrieveRemoteImage(string src)
         {
-            using (WebClient client = new WebClient())
+            var message = new HttpRequestMessage(HttpMethod.Get, src);
+            AddCookiesToMessage(src, message);
+
+            var response = HttpClientSingleton.Instance.SendAsync(message).Result;
+
+            if (response.IsSuccessStatusCode)
             {
-                var imageBytes = client.DownloadData(src);
+                var bytes = response.Content.ReadAsByteArrayAsync().Result;
 
-                if (imageBytes != null)
+                Trace.TraceInformation("File downloaded: {0} {1} bytes", src, bytes.LongLength);
+
+                try
                 {
-                    return Image.GetInstance(imageBytes);
+                    return Image.GetInstance(bytes);
                 }
-
-                return null;
+                catch (Exception ex)
+                {
+                    Trace.TraceError("Error creating image {0}: {1}", src, ex.Message);
+                }
             }
+            else
+            {
+                Trace.TraceInformation("Error downloading: {0} {1}", src, response.StatusCode);
+            }
+
+
+            return null;
+
         }
     }
 }
